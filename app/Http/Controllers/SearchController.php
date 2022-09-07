a0013adf68e0defc9664046178488112a59bdca2
Farhod Vokhidov <farhodvokhidov9723@gmail.com>
2022-09-08 00:44:20 +0500

    User search select2

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function index(){
+        return view('searchDemo');
+    }
+    public function autocomplete(Request $request){
+        $data = [];
+
+        if($request->filled('q')){
+            $data=User::query()->select('name','id')
+                ->where('name','LIKE','%'. $request->get('q').'%')
+                ->get();
+        }
+        return response()->json($data);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(\App\Http\Controllers\SearchController::class)->group(function (){
+    Route::get('/demo-search','index');
+    Route::get('/autocomplete','autocomplete')->name('autocomplete');
 });
